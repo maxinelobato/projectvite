12d1bbe4ef81ef4adb31abc750be06ed053797be
maxinelobato <maxinelobato89@hotmail.com>
2022-09-24 01:00:49 -0300

    colormode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, theme } from "@chakra-ui/react";
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import { CTAHelp } from "./components/Buttons/CTAHelp";
 
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+const customTheme = extendTheme({ config });
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <CTAHelp />
       <App />
     </ChakraProvider>
